c0827fb27c5833597e6f786cfa4b90781d9c0bca
Julien Dutant <34026710+jdutant@users.noreply.github.com>
2025-01-09 13:28:30 +0100

    Release recursive-citeproc 2.0.2

--- _extensions/recursive-citeproc/recursive-citeproc.lua
+++ _extensions/recursive-citeproc/recursive-citeproc.lua
@@ -379,7 +379,7 @@ bibliographies in Pandoc and Quarto
 @author Julien Dutant <julien.dutant@kcl.ac.uk>
 @copyright 2021-2024 Julien Dutant
 @license MIT - see LICENSE file for details.
-@release 2.0.1
+@release 2.0.2
 ]]
 
 local log = require('log')
